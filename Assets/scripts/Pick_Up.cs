7405f239a465fb667d04591e790bc66ab7646643
Anthony Robinson <aerobins12994@gmail.com>
2016-12-21 14:45:35 -0500

    level transition update

--- Assets/scripts/Pick_Up.cs
+++ Assets/scripts/Pick_Up.cs
@@ -8,8 +8,12 @@ public class Pick_Up : MonoBehaviour {
     public GameObject fadedOutItem1;
     public GameObject pickedUpItem1;
     public GameObject getTotalAmmo;
+    public GameObject resetHealth;
     public int distance;
     public int item_count = 0;
+    public int item_count2 = 0;
+    public int item_count3 = 0;
+    
     
 
 	// Use this for initialization
@@ -27,6 +31,7 @@ public class Pick_Up : MonoBehaviour {
     {
       if(Input.GetMouseButtonUp(1))
         {
+            //Debug.Log(item_count);
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -37,21 +42,58 @@ public class Pick_Up : MonoBehaviour {
                     Debug.Log("item hit");
                     Destroy(hit.collider.gameObject);
                     item_count++;
+                    Debug.Log(item_count);
                     fadedOutItem1.SetActive(false);
                     pickedUpItem1.SetActive(true);
                 }
-                if(hit.collider.gameObject.name == "Ammo")
+                if (hit.collider.gameObject.name == "item1")
+                {
+                    Debug.Log("item1 hit");
+                    Destroy(hit.collider.gameObject);
+                    item_count2++;
+                    
+                    fadedOutItem1.SetActive(false);
+                    pickedUpItem1.SetActive(true);
+                }
+
+                if (hit.collider.gameObject.name == "item2")
+                {
+                    Debug.Log("item1 hit");
+                    Destroy(hit.collider.gameObject);
+                    item_count3++;
+
+                    fadedOutItem1.SetActive(false);
+                    pickedUpItem1.SetActive(true);
+                }
+                if (hit.collider.gameObject.name == "Ammo_pack")
                 {
                     Debug.Log("Ammo hit");
-                    Destroy(hit.collider.gameObject);
                     getTotalAmmo.GetComponent<RayCast_Shoot>().totalAmmo = 50;
+                    Destroy(hit.collider.gameObject);
                     Debug.Log(getTotalAmmo);
+                }
+                if (hit.collider.gameObject.name == "health")
+                {
+                    Debug.Log("health hit");
+                    resetHealth.GetComponent<PlayerHealth>().currentHealth = 100;
+     
+                    Destroy(hit.collider.gameObject);
+                    fadedOutItem1.SetActive(false);
+                    pickedUpItem1.SetActive(true);
                 }
             }
 
             if(item_count == 1)
             {
                 SceneManager.LoadSceneAsync("demoLevel2");
+            }
+            if(item_count2 == 2)
+            {
+                SceneManager.LoadSceneAsync("demoLevel3");
+            }
+            if(item_count3 == 4)
+            {
+                SceneManager.LoadSceneAsync("gameWon");
             }
         }
     }
---
+++ Assets/scripts/counter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class counter
+{
+
+    public static int count = 0;
+}
+	
